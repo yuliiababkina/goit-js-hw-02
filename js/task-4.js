802d7918d13f96c3a03c84f5dd767d07654e68b4
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2024-08-31 09:49:23 +0300

    refactoring goit-js-hw-02

--- js/task-4.js
+++ js/task-4.js
@@ -4,16 +4,16 @@ function getShippingCost(country) {
     switch (country) {
         case "China":
             return `Shipping to ${country} will cost 100 credits`;
-            break;
+
         case "Chile":
             return `Shipping to ${country} will cost 250 credits`;
-            break;
+
         case "Australia":
             return `Shipping to ${country} will cost 170 credits`;
-            break;
+
         case "Jamaica":
             return `Shipping to ${country} will cost 120 credits`;
-            break;
+
         default:
             return `Sorry, there is no delivery to your country`;
     }
